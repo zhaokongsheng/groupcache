4a378204a6a8662abd7b6c5537619f02ef681824
Wuvist <wuvist@gmail.com>
2013-11-25 16:02:06 +0800

    Add group delete with ServeMux

--- http.go
+++ http.go
@@ -138,6 +138,10 @@ func (p *HTTPPool) PickPeer(key string) (ProtoGetter, bool) {
 	return nil, false
 }
 
+func (p *HTTPPool) GetAllPeers() (peer []ProtoGetter) {
+	return p.peerGetters
+}
+
 func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Parse request.
 	if !strings.HasPrefix(r.URL.Path, p.opts.BasePath) {
@@ -181,7 +185,7 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/x-protobuf")
 		w.Write(body)
 	case "DELETE":
-		group.Remove(key)
+		group.removeByPeer(key)
 	}
 }
 
@@ -201,7 +205,7 @@ func (h *httpGetter) Get(context Context, in *pb.GetRequest, out *pb.GetResponse
 		url.QueryEscape(in.GetGroup()),
 		url.QueryEscape(in.GetKey()),
 	)
-	req, err := http.NewRequest("GET", u, nil)
+	req, err := http.NewRequest(method, u, nil)
 	if err != nil {
 		return err
 	}
@@ -229,4 +233,12 @@ func (h *httpGetter) Get(context Context, in *pb.GetRequest, out *pb.GetResponse
 		return fmt.Errorf("decoding response body: %v", err)
 	}
 	return nil
+}
+
+func (h *httpGetter) Get(context Context, in *pb.GetRequest, out *pb.GetResponse) error {
+	return h.call(context, in, out, "GET")
+}
+
+func (h *httpGetter) Delete(context Context, in *pb.GetRequest, out *pb.GetResponse) error {
+	return h.call(context, in, out, "DELETE")
 }
